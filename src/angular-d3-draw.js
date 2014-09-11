882f917cb89e14b5042e1fabfe44982d02fe17c5
wind13 <wind13@163.com>
2014-09-11 22:55:12 +0800

    need to fix loop forever.

--- src/angular-d3-draw.js
+++ src/angular-d3-draw.js
@@ -38,7 +38,9 @@
                     rectClass: '@'
                 },
                 controller: function($scope) {
-                    $scope.padding = 10;
+                    $scope.MIN_FONT_SIZE = 12;
+                    $scope.MAX_FONT_SIZE = 48;
+                    $scope.PADDING = 10;
                     $scope.lines = function() {
                         console.log("width:" + $scope.width + ";");
                         console.log("fontsize:" + $scope.fontsize() + ";");
@@ -48,6 +50,25 @@
                         console.log("ls:" + ls + ";");
                         return ls;
                     };
+                    $scope.calculateLineHeight = function(element) {
+                        var lineHeight = parseInt($(element).css('line-height'), 10);
+                        var clone;
+                        var singleLineHeight;
+                        var doubleLineHeight;
+
+                        if (isNaN(lineHeight)) {
+                            clone = element.cloneNode();
+                            clone.innerHTML = '<br>';
+                            element.appendChild(clone);
+                            singleLineHeight = clone.offsetHeight;
+                            clone.innerHTML = '<br><br>';
+                            doubleLineHeight = clone.offsetHeight;
+                            element.removeChild(clone);
+                            lineHeight = doubleLineHeight - singleLineHeight;
+                        }
+
+                        return lineHeight;
+                    }
                     $scope.fontsize = function() {
                         $scope.textcount = $scope.text.length; //TODO check Chinese char to 2*l;
                         console.log("textcount:" + $scope.textcount + ";");
@@ -56,10 +77,11 @@
                         console.log("zmj:" + zmj + ";");
                         var fz = Math.round(zmj);
                         console.log("fz:" + fz + ";");
-                        return fz;
+                        console.log("fz:" + fz + ";");
+                        return Math.min(fz, $scope.MIN_FONT_SIZE);
                     };
                     $scope.lineheight = function() {
-                        return Math.floor(($scope.height - $scope.padding) / $scope.lines());
+                        return Math.floor(($scope.height - $scope.PADDING) / $scope.lines());
                     };
                 },
                 link: function(scope, element, attr, svgCtrl) {
@@ -78,20 +100,88 @@
                     fob.attr("width", attr.width);
                     fob.attr("height", attr.height);
                     var style = fob.append("xhtml:style");
-                    style.text(".rectText { text-align: center; font-size: " + scope.fontsize() +
-                        "px; padding: " + scope.padding + "px; box-sizing: border-box; line-height: " +
-                        scope.lineheight() + "px; height: inherit; overflow: hidden; }" +
+                    //  height: inherit; overflow: hidden;
+                    // font-size: " + scope.fontsize() + "px;
+                    // line-height: " + scope.lineheight() + "px; 
+                    style.text(".rectText { text-align: center; padding: " + scope.PADDING + "px; box-sizing: border-box;}" +
                         " .rectStyle { fill: #f60; stroke: black; stroke-width: 5; opacity: 0.3 }");
                     var div = fob.append("xhtml:div");
                     div.attr("id", g.attr("id") + "text");
                     div.attr("class", "rectText " + attr.textClass);
                     div.attr("title", attr.text);
+                    div.attr("data-fittext", "");
                     div.text(attr.text);
+                    console.log($(div[0]).height());
                     scope.$watch('text', function(newValue, oldValue) {
-                        d3.select("#" + scope.id + "text").text(newValue).style({
+                        var divtext = d3.select("#" + scope.id + "text");
+                        divtext.style({
+                            "overflow": "display",
+                            "height": "auto"
+                        });
+                        divtext.text(newValue);
+                        var hdiv = $(div[0]).height() + scope.PADDING * 2;
+                        var hrct = parseInt(rct.attr("height"));
+                        var lh = scope.calculateLineHeight(div[0][0]);
+                        var fz = parseInt($(div[0]).css("font-size"));
+                        var fzn, lhn;
+                        console.log(hdiv + " vs " + hrct);
+                        if (hdiv > 0 && hrct > 0 && lh > 0 && fz > 0) {
+                            // Set fz and lh not over confine.
+                            fzn = Math.max(fz, scope.MIN_FONT_SIZE);
+                            fzn = Math.min(fzn, scope.MAX_FONT_SIZE);
+                            lhn = Math.min(lh, hrct - scope.PADDING * 2);
+                            lhn = Math.max(lhn, fz);
+                            if (fzn != fz || lhn != lh) {
+                                divtext.style({
+                                    "font-size": fzn + "px",
+                                    "line-height": lhn + "px"
+                                });
+                                hdiv = $(div[0]).height() + scope.PADDING * 2;
+                            }
+
+                            // Adjust fz and lh to fit the rect.
+                            while (hdiv > hrct) {
+                                if (lhn <= fzn * 1.5 && fzn >= scope.MIN_FONT_SIZE + 2) {
+                                    fzn = fzn - 1;
+                                    divtext.style({
+                                        "font-size": fzn + "px"
+                                    });
+                                } else {
+                                    if (lhn <= scope.MIN_FONT_SIZE) {
+                                        divtext.style({
+                                            "overflow": "hidden",
+                                            "height": hrct + "px"
+                                        });
+                                        return;
+                                    } else {
+                                        lhn = lhn - 1;
+                                        divtext.style({
+                                            "line-height": lhn + "px"
+                                        });
+                                    }
+                                }
+                                hdiv = $(div[0]).height() + scope.PADDING * 2;
+                            }
+                            while (hdiv < hrct) {
+                                if (fzn >= scope.MAX_FONT_SIZE || lhn < fzn * 1.5) {
+                                    lhn = lhn + 1;
+                                    divtext.style({
+                                        "line-height": lhn + "px"
+                                    });
+                                } else {
+                                    fzn = fzn + 1;
+                                    divtext.style({
+                                        "font-size": fzn + "px"
+                                    });
+                                }
+                                hdiv = $(div[0]).height() + scope.PADDING * 2;
+                            }
+                        }
+                        divtext.attr("title", newValue);
+                        /* .style({
                             "font-size": scope.fontsize()+"px",
                             "line-height": scope.lineheight() +"px"
-                        });
+                        }); */
                     });
                 }
             };
